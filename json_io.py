442317bdd1aca10558d6f9b1dc94a1b149f2d2ca
Shakhemir <shakhemir@gmail.com>
2023-03-12 19:13:53 +0300

    Обработка возможных ошибок если файл пустой или отсутствует

--- json_io.py
+++ json_io.py
@@ -3,12 +3,18 @@ from config import json_file
 
 
 def read_json():
-    with open(json_file, encoding='utf-8') as f:
-        read_file = json.loads(f.read())
-    return read_file
+    try:
+        with open(json_file) as f:
+            read_file = json.loads(f.read())
+    except json.JSONDecodeError:
+        return []
+    except FileNotFoundError:
+        open(json_file, 'w').close()
+        return []
+    else:
+        return read_file
 
 
 def write_json(notes_list):
-    with open(json_file, 'w', encoding='utf-8') as f:
+    with open(json_file, 'w') as f:
         f.write(json.dumps(notes_list, indent=4, default=str, ensure_ascii=False))
-
